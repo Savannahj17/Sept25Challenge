6f77179c1870b028152fbca9dcad4fb9d4be0165
Savannah Jackson <savannahjackson33@gmail.com>
2021-09-25 16:11:20 -0400

    Final Commit

--- Sept25Challenge/Sept25Challenge.cs
+++ Sept25Challenge/Sept25Challenge.cs
@@ -10,7 +10,62 @@ namespace Sept25Challenge
     {
         static void Main(string[] args)
         {
+            List<char> super = new List<char>();
+            super.Add('s');
+            super.Add('u');
+            super.Add('P');
+            super.Add('e');
+            super.Add('r');
+            super.Add('c');
+            super.Add('a');
+            super.Add('l');
+            super.Add('i');
+            super.Add('f');
+            super.Add('r');
+            super.Add('a');
+            super.Add('g');
+            super.Add('i');
+            super.Add('l');
+            super.Add('i');
+            super.Add('s');
+            super.Add('t');
+            super.Add('i');
+            super.Add('c');
+            super.Add('e');
+            super.Add('x');
+            super.Add('p');
+            super.Add('i');
+            super.Add('a');
+            super.Add('l');
+            super.Add('i');
+            super.Add('d');
+            super.Add('o');
+            super.Add('c');
+            super.Add('i');
+            super.Add('o');
+            super.Add('u');
+            super.Add('s');
+            foreach(char x in super)
+            {
+                if (x != 'i')
+                {
+                    Console.WriteLine("Not an I");
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
 
+            }
+
+            {
+                string a = "supercalifragilisticexpialidocious";
+                int b = a.Length;
+                Console.WriteLine(b);
+            }
+            Console.ReadKey();
         }
+
+       
     }
 }
